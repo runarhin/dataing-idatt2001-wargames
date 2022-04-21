4df5e8283a3dd4c9d24f0f58ba9f1fe3a8c418d6
Runar Halvorsen Indahl <runarin@stud.ntnu.no>
2022-04-21 15:09:01 +0200

    feat(main...exceptions): Created ArmyEmptyOfUnitsException to notify a user that an army must be initialised from a file.

---
+++ src/main/java/edu/ntnu/idatt2001/runarin/wargames/backend/exceptions/ArmyEmptyOfUnitsException.java
@@ -0,0 +1,24 @@
+package edu.ntnu.idatt2001.runarin.wargames.backend.exceptions;
+
+import java.io.IOException;
+
+/**
+ * Exception handling for showing dialog window in the War Games GUI.
+ * This exception is thrown when the system tries to initialise a simulation
+ * while an army has no units.
+ *
+ * @author Runar Indahl
+ * @version 3.0
+ * @since 2022-04-21
+ */
+public class ArmyEmptyOfUnitsException extends IOException {
+    /**
+     * Instantiates a new ArmyEmptyOfUnitsException-object inherited by IOException super class.
+     * Specifies the Exception message.
+     *
+     * @param message message from the super class, IOException.
+     */
+    public ArmyEmptyOfUnitsException(String message) {
+        super(message);
+    }
+}
