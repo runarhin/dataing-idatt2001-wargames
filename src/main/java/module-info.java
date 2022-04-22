15c5d85fb32d722221878d33199b1f39b181c05c
Runar Halvorsen Indahl <runarin@stud.ntnu.no>
2022-04-22 08:39:40 +0200

    feat(main...module-info.java): Adding requirements and exports to module-info.java to enable use of GUI classes.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,9 @@
 module wargames {
     requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
 
+    opens edu.ntnu.idatt2001.runarin.wargames.frontend.controllers to javafx.fxml;
     exports edu.ntnu.idatt2001.runarin.wargames.frontend;
+    exports edu.ntnu.idatt2001.runarin.wargames.frontend.views to javafx.graphics;
 }
